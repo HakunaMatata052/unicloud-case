ec52368d15039d30f9fc145ce263a5a670f25e1b
calvin wong <289016698@qq.com>
2019-09-18 11:04:26 +0800

    first_admin

---
+++ backend/weimumu_admin/src/main/java/top/weimumu/weimumu/WeimumuApplication.java
@@ -0,0 +1,13 @@
+package top.weimumu.weimumu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeimumuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeimumuApplication.class, args);
+    }
+
+}
